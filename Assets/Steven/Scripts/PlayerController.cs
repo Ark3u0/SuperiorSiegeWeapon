3b89fa180d45b0f292034578558c678ccac77d3a
ark3u0 <sgarcia.software@gmail.com>
2020-09-21 20:19:00 -0700

    Put Player and Ball on different layers to control physics interaction; only allow interactableCollider to interact

--- Assets/Steven/Scripts/PlayerController.cs
+++ Assets/Steven/Scripts/PlayerController.cs
@@ -78,8 +78,8 @@ public class PlayerController : MonoBehaviour
 
         if (direction.magnitude >= 0.1f) {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            // float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
 
             toMove = (Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward).normalized * speed * Time.deltaTime;
         }
