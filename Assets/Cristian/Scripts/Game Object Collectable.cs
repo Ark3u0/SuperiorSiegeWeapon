f90606fd0c9fe67b1cb88e709bcc1b09e07b3e8a
Cristian Guzman <cristianguzman@protonmail.com>
2020-09-23 18:10:13 -0700

    New bulding prefabs and update to level
    add a few building prefabs and a Jaripeo Ring to the level

---
+++ Assets/Cristian/Scripts/Game Object Collectable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectCollectable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {/*
+       // PlayerController controller = other.gameObject().GetComponent<>();
+        if (controller)
+        {
+            // call an add Object Integer function in the player controller function 
+            //Player Audio Feedback for pick up 
+            //Dealte self drom the game 
+           // delete(self); 
+        }*/
+
+    }
+}
