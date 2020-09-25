758760aad7bf05851478083a97f64431ddef639b
ark3u0 <sgarcia.software@gmail.com>
2020-09-24 18:21:30 -0700

    Add interactionBox manager for controlling when to show interactions

--- Assets/Steven/Scripts/Player/InteractableCollider.cs
+++ Assets/Steven/Scripts/Player/InteractableCollider.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class InteractableCollider : MonoBehaviour
 {
+    public InteractionBoxManager interactionBoxManager;
     public CharacterController controller;
 
     public void OnTriggerEnter(Collider other) {
         Ball ball = other.gameObject.GetComponent<Ball>();
         if (ball != null) {
+            interactionBoxManager.ShowInteraction();
             controller.GetComponent<PlayerController>().SetBall(ball);
         }
 
         NpcController npc = other.gameObject.GetComponent<NpcController>();
         if (npc != null) {
+            interactionBoxManager.ShowInteraction();
             controller.GetComponent<PlayerController>().SetNpc(npc);     
         }
     }
@@ -21,11 +24,13 @@ public class InteractableCollider : MonoBehaviour
     public void OnTriggerExit(Collider other) {
         Ball ball = other.gameObject.GetComponent<Ball>();
         if (ball != null) {
+            interactionBoxManager.HideInteraction();
             controller.GetComponent<PlayerController>().SetBall(null);
         }
 
         NpcController npc = other.gameObject.GetComponent<NpcController>();
         if (npc != null) {
+            interactionBoxManager.HideInteraction();
             controller.GetComponent<PlayerController>().SetNpc(null);     
         }
     }
