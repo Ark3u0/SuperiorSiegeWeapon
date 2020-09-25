bcc4432fafea8f1a87725359f7f1e033c78a1ff1
Steven Garcia <steven.garcia@mcg.com>
2020-09-24 22:24:40 -0700

    Anchor UI elements properly for different screen sizes; Set up first pass at interactionBox manager

--- Assets/InteractionBoxManager.cs
+++ Assets/InteractionBoxManager.cs
@@ -1,13 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class InteractionBoxManager : MonoBehaviour
 {
     public GameObject interactionBox;
+    public Sprite aButton;
+    public Sprite rightTrigger;
+    public Text interaction;
+    public Image input;
 
-    public void ShowInteraction() {
+    private Sprite GetSprite(string interactionText) {
+        switch (interactionText.ToLower())
+        {
+            case "aim":
+            case "kick":
+                return rightTrigger;
+            case "talk":
+                return aButton;
+            default:
+                return null;
+        }
+    }
+
+    public void ShowInteraction(string interactionText) {
         interactionBox.SetActive(true);
+        interaction.text = interactionText;
+        input.sprite = GetSprite(interactionText);
     }
 
     public void HideInteraction() {
