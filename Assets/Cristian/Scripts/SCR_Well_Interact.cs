6429ebe6bc4603c96c6f2e3d7497acfdbd555db6
Cristian Guzman <cristianguzman@protonmail.com>
2020-09-27 13:58:37 -0700

    Adding in new Puzzle Prefabs
    Add new puzzle Prefabs like Tree and Well puzzle

---
+++ Assets/Cristian/Scripts/SCR_Well_Interact.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_Well_Interact : MonoBehaviour
+{
+
+    /// <summary>
+    /// These variables are made for the ball launch and a spawn of collectabless
+    /// </summary>
+    [SerializeField] public float launchForce; 
+    [SerializeField] ParticleSystem wellEffect = null;
+    [SerializeField] public GameObject collectable;
+    [SerializeField] public GameObject CollectableSpawnLocation;
+
+
+    private Vector3 CollectableSpawnPoint; 
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        CollectableSpawnPoint = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    // reads when the ball enters the collition box
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 9f)// 9 is the ball layer
+        {
+            Debug.Log("Ball in well");
+            //BallInWellEffect(other.gameObject);
+            StartCoroutine(BallInWellEffect(other.gameObject));
+        }
+        else
+        {
+            Debug.Log("Ball not in well");
+        }
+
+    }
+
+
+    // effect for when the ball enters the collision box is fired
+    private IEnumerator BallInWellEffect(GameObject ball)
+    {
+
+        yield return new WaitForSeconds(3f);
+        // audio effect
+
+
+        // ball audio effect 
+        ball.GetComponent<Rigidbody>().AddForce(Vector3.up * launchForce);
+        wellEffect.Play();
+
+
+        // insted of ball get the collectable
+        GameObject Prize = Instantiate(collectable);
+
+        Prize.transform.position = new Vector3(CollectableSpawnPoint.x, 
+            CollectableSpawnPoint.y, CollectableSpawnPoint.z);
+
+        Prize.GetComponent<Rigidbody>().AddForce(CollectableSpawnLocation.transform.up * launchForce );
+
+    }
+
+
+
+}
