beb01d919f1c3cffd80f846c56fcc86eec4f8660
Steven Garcia <sgarcia@thoughtworks.com>
2020-10-03 12:27:54 -0700

    Add fade in/out support for end screen'

---
+++ Assets/Steven/Scripts/Util/FadeInManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// https://www.youtube.com/watch?v=cmYpkqrqhrA
+public class FadeInManager : MonoBehaviour
+{
+    public Image fadeImage;
+    public Color startColor;
+    public Color endColor;
+    public float duration;
+    public bool isFading;
+
+    void Start() {
+        FadeIn();
+    }
+
+    public void FadeIn() {
+        StartCoroutine(BeginFade());
+    }
+
+    private IEnumerator BeginFade() 
+    {
+        isFading = true;
+
+        float timer = 0f;
+
+        while (timer <= duration) 
+        {
+            fadeImage.color = Color.Lerp(startColor, endColor, timer / duration);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Steven/Scripts/Util/FadeOutManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// https://www.youtube.com/watch?v=cmYpkqrqhrA
+public class FadeOutManager : MonoBehaviour
+{
+    public Image fadeImage;
+    public Color startColor;
+    public Color endColor;
+    public float duration;
+    public bool isFading;
+
+    public void FadeOut(Fadeable fadeable) {
+        StartCoroutine(BeginFade(fadeable));
+    }
+
+    private IEnumerator BeginFade(Fadeable fadeable) 
+    {
+        isFading = true;
+
+        float timer = 0f;
+
+        while (timer <= duration) 
+        {
+            fadeImage.color = Color.Lerp(startColor, endColor, timer / duration);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        isFading = false;
+
+        fadeable.RunPostFade();
+    }
+}
